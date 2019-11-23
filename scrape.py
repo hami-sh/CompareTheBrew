e626dbc71d196dd299a76ace1e60d69e5dac24b3
Hamish <sendanemailtohamish@gmail.com>
2019-11-23 22:30:01 +1000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -29,6 +29,7 @@ def download(url, target_filename, filename_extension, total, listBWS):
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     return soup
 
+
 def get_drinks(soup, liquorSite):
     """
     Function to get a list of drink data from a BeautifulSoup and return the data in a list
@@ -38,18 +39,18 @@ def get_drinks(soup, liquorSite):
 
     Returns: A list of drink data
     """
+    drinks = list()
     # Get the drinks profiles based on the given liquorSite
     if liquorSite == "bws":
         # Extract the drink profiles from the BeautifulSoup (configured for bws)
         drinks = soup.findAll('div', {'class':'productTile'})
-    else if liquorSite == "liquorland":
+    elif liquorSite == "liquorland":
         # Extract the drink profiles from the BeautifulSoup (configured for liquorland)
         specials = soup.findAll('div', {'class':'product-tile-wrapper update-specials-border'})
         drinks = soup.findAll('div', {'class': 'product-tile-wrapper'})
         drinks.append(specials)
 
-    # Extract the drink profiles from the BeautifulSoup (configured for bws)
-    drinks = soup.findAll('div', {'class':'productTile'})
+
     # Print all of the drinks profiles
     print('SCRAPED ' + str(len(drinks)))
     # Threading stuff basically executes multiple copies item_thread_bws(item, listBWS) concurrently
@@ -58,51 +59,10 @@ def get_drinks(soup, liquorSite):
         for item in drinks:
             print("INIT_THREAD[" + str(threads) + "]")
             threads += 1
-            # Run item_thread_bws(item, listBWS)
-            executor.submit(item_thread_bws, item, listBWS)
+            if liquorSite == "bws":
+                # Run item_thread_bws(item, listBWS)
+                executor.submit(item_thread_bws, item, listBWS)
+            elif liquorSite == "liquorland":
+                executor.submit(item_thread_liquorland, item, listBWS)
 
-def item_thread_bws(item, listBWS):
-    """
-    Thread function to control parsing of BWS drink details
-    """
-    # brand
-    brand = item.find('h2', {'class': 'productTile_brand ng-binding'})
-    # name
-    name = item.find('div', {'class': 'productTile_name ng-binding'})
-    # price
-    dollar = item.find('span', {'class': 'productTile_priceDollars ng-binding'})
-    cents = item.find('span', {'class': 'productTile_priceCents ng-binding'})
-    price = str(dollar.text) + '.' + str(cents.text)
-    # link
-    link = item.find('a', {'class':'link--no-decoration'})
 
-    # alcohol content
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://bws.com.au" + link['href'])
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    detailsRaw = soup.find('div', {'class':'product-additional-details_container text-center ng-isolate-scope'})
-    list = detailsRaw.find('ul', {'class':'text-left'})
-    keys = list.findAll('strong', {'class':'list-details_header ng-binding'})
-    values = list.findAll('span', {'class':'pull-right list-details_info ng-binding ng-scope'})
-    details = dict()
-
-    for x in range(0, len(keys)):
-        details[keys[x].text] = values[x].text
-
-    size = 0
-    if details['Liquor Size'].find('mL') != -1:
-        # measurement in mL
-        strSize = details['Liquor Size'][0:len(details['Liquor Size']) - 2]
-        size = int(strSize) / 1000
-    else:
-        # measurement in L
-        strSize = details['Liquor Size'][0:len(details['Liquor Size']) - 1]
-        size = int(strSize)
-
-    efficiency = float(details['Standard Drinks']) / float(price)
-    entry = Item("BWS", brand.text, name.text, price, "https://bws.com.au" + link['href'], details['Liquor Size'],
-                 details['Alcohol %'], details['Standard Drinks'], efficiency)
-    print(entry.name + " " + entry.stdDrinks + " " + entry.price + " " + str(size) + " " + str(efficiency))
-    listBWS.append(entry)
