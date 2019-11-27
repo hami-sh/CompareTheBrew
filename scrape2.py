e2e0e4b261811d7a23ba208c83f0b8139506f1e0
Anwealso <alnicholo@gmail.com>
2019-11-28 09:14:08 +1000

    Fixed liquorland urls list

--- scrape2.py
+++ scrape2.py
@@ -16,7 +16,7 @@ from threading import Lock
 The urls we will need to scrape to populate our database:
     bwsPopulateUrls = {'beer':'https://bws.com.au/beer/all-beer', 'wine':'https://bws.com.au/wine/all-wine', 'spirits':'https://bws.com.au/spirits/all-spirits'}
 
-    liquorlandPopulateUrls = {'beer':'https://www.liquorland.com.au/beer', 'red-wine':'https://www.liquorland.com.au/wine', 'spirits':'https://www.liquorland.com.au/spirits'}
+    liquorlandPopulateUrls = {'beer':'https://www.liquorland.com.au/beer', 'wine':'https://www.liquorland.com.au/wine', 'spirits':'https://www.liquorland.com.au/spirits'}
 
     danmurphysPopulateUrls = {'beer':'https://www.danmurphys.com.au/beer/all', 'wine':'https://www.danmurphys.com.au/list/wine', 'spirits':'https://www.danmurphys.com.au/spirits/all'}
 
