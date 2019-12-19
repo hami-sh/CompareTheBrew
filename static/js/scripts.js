6caa907cb3b191909a52bd49dfdbae0e726f4fc3
Matthew Costello <mcost45@hotmail.com>
2019-12-20 02:27:22 +1000

    search button func.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,4 +2,8 @@ $(document).ready(function(e) {
         document.getElementById("slide").addEventListener('click', function() {
                 document.getElementById("cover").classList.toggle("shadow");
         });
+
+        document.getElementById("searchButton").addEventListener('click', function() {
+                document.getElementById("query").submit();
+        });
 });
