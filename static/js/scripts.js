d07879bc862648a7a06811b868a8cc7cefe75252
Matthew Costello <mcost45@hotmail.com>
2019-12-20 18:50:45 +1000

    start of lots of general improvements/additions

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,9 +1,16 @@
 $(document).ready(function(e) {
         document.getElementById("slide").addEventListener('click', function() {
                 document.getElementById("cover").classList.toggle("shadow");
+                document.getElementById("cover").classList.toggle("clickable");
         });
 
         document.getElementById("searchButton").addEventListener('click', function() {
                 document.getElementById("query").submit();
+        });
+
+        document.getElementById("cover").addEventListener('click', function() {
+                document.getElementById("slide").checked = false;
+                document.getElementById("cover").classList.toggle("shadow");
+                document.getElementById("cover").classList.toggle("clickable");
         });
 });
