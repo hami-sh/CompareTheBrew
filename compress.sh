85e1e934a49d9346c5a7082fffa1cc2f71e56f7c
Ubuntu <ubuntu@ip-172-26-6-6.ap-southeast-2.compute.internal>
2019-12-24 07:40:15 +0000

    merge back to master

--- compress.sh
+++ compress.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 imagedir="/home/ubuntu/CompareTheBrewDev/static/images"
-for file in $imagedir/uncompressed/*; do
-        echo "$file"
-	cwebp -q 80 "$file" -o "$file.webp"
-	mv "$file.webp" "$imagedir/drinkimages/"
-done
-
-rm -rf $imagedir/uncompressed/*
+cd $imagedir/uncompressed
+mogrify -format png *.webp
+#rm -rf $imagedir/uncompressed/*
--- pipeline.sh
+++ pipeline.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 workdir="/home/ubuntu/CompareTheBrewDev"
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
 # begin - scrape bws website
 ## beer
 n=0
 until [ $n -ge 5 ]
 do 
-   #echo "$(date +"%Y-%m-%d %T")"
+   echo "${GREEN}BEER <$(date +"%T")>${NC}"
    #python3 ./scrape.py bws beer 0 > "$(date +"%Y-%m-%d %T")".log 2> "$(date +"%Y-%m-%d %T")".err && break
-   time="$(date +"%Y-%m-%d")"
+   time="$(date +"%d-%m-%Y")"
+   echo "start"
    log="$workdir/log/beer-$time.log"
    err="$workdir/log/beer-$time.err"
-   cmd="$(sudo python3 $workdir/scrape.py bws beer 0)"
-   "$cmd" #> "$log" 2> "$err" && break
+   sudo python3 $workdir/scrape.py bws beer 0 && break
    n=$[$n+1]
    sleep 5
 done
@@ -20,14 +22,13 @@ done
 ## wine
 n=0
 until [ $n -ge 5 ]
-do
-   #echo "$(date +"%Y-%m-%d %T")"
-   #python3 ./scrape.py bws beer 0 > "$(date +"%Y-%m-%d %T")".log 2> "$(date +"%Y-%m-%d %T")".err && break
-   time="$(date +"%Y-%m-%d")"
+do  
+   echo "${GREEN}WINE <$(date +"%T")>${NC}"
+   #python3 ./scrape.py bws beer 0 > "$(date +"%Y-%m-%d %T")".log 2> "$(date +"%Y-%m-%d %T")".err && break   
+   time="$(date +"%d-%m-%Y")"
    log="$workdir/log/wine-$time.log"
    err="$workdir/log/wine-$time.err"
-   cmd="$(python3 $workdir/scrape.py bws wine 0)"
-   echo "$cmd" > "$log" 2> "$err" && break
+   sudo python3 $workdir/scrape.py bws wine 0 && break
    n=$[$n+1]
    sleep 5
 done
@@ -35,14 +36,14 @@ done
 ## spirits
 n=0
 until [ $n -ge 5 ]
-do
+do  
+   echo "${GREEN}SPIRITS <$(date +"%T")>${NC}"
    #echo "$(date +"%Y-%m-%d %T")"
    #python3 ./scrape.py bws beer 0 > "$(date +"%Y-%m-%d %T")".log 2> "$(date +"%Y-%m-%d %T")".err && break
    time="$(date +"%Y-%m-%d")"
    log="$workdir/log/spirits-$time.log"
-   err="$workdir/log/spirits-$time.err"
-   cmd="$(python3 $workdir/scrape.py bws spirits 0)"
-   echo "$cmd" > "$log" 2> "$err" && break
+   err="$workdir/log/spirits-$time.err" 
+   sudo python3 $workdir/scrape.py bws spirits 0 && break
    n=$[$n+1]
    sleep 5
 done
@@ -55,14 +56,17 @@ done
 
 # download images
 time="$(date +"%Y-%m-%d")"
-python3 imagedl.py > "$workdir/log/image-$time.log"
+echo "${GREEN}IMAGES${NC}"
+sudo python3 imagedl.py #> "$workdir/log/image-$time.log"
 
 # convert to webp format
+echo "${GREEN}PNG CONVERT${NC}"
 imagedir="/home/ubuntu/CompareTheBrewDev/static/images"
+cd $imagedir/uncompressed
+sudo mogrify -format png *.webp
 for file in $imagedir/uncompressed/*; do
         echo "$file"
-        cwebp -q 80 "$file" -o "$file.webp"
-        mv "$file.webp" "$imagedir/drinkimages/"
+	mv "$file.png" "$imagedir/drinkimages/"
 done
 
 ## delete extra images that have otherwise been compressed
